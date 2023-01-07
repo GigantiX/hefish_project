5190cd3ccdb6ea9485af3baa283b00ea73e9540a
GigantiX <axel.ganendra157@gmail.com>
2023-01-07 19:56:28 +0700

    Logout system fixed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -18,6 +19,11 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  clearToken() async{
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    await preferences.clear();
+  }
+
   @override
   void initState() {
     // TODO: implement initState
@@ -28,6 +34,54 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Color(0xff0EA7AE),
+        title: Text('He Fish'),
+        actions: [
+          PopupMenuButton(
+            icon: Icon(Icons.settings_rounded),
+            color: Colors.white,
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                onTap: () => Future.delayed(
+                  Duration(seconds: 0),
+                    () =>AwesomeDialog(
+                            context: context,
+                            width: 300,
+                            dialogType: DialogType.question,
+                            title: 'Log out',
+                            desc: 'Do you want to logout?',
+                            btnOkText: 'Yes',
+                            btnOkColor: Colors.green,
+                            btnOkOnPress: () {
+                              clearToken();
+                              Navigator.pushNamedAndRemoveUntil(context,'/welcome',
+                                      (Route<dynamic> route) => false
+                              );
+                            },
+                            btnCancelText: 'No',
+                            btnCancelColor: Colors.red,
+                            btnCancelOnPress: (){}
+                        ).show()
+                ),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.logout,
+                        color: Colors.black,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text('Logout')
+                    ],
+                  )
+              )
+            ],
+          )
+        ],
+      ),
       body: Container(
         child: Center(
           child: Text(myToken),
@@ -35,5 +89,4 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-
 }
